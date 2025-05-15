446c7edd00cff874e0af8a288c86812f147f136b
Pottassiumm <6554@leerling.haganum.nl>
2025-05-15 20:21:44 +0200

    First commit

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+const fs = require('fs');
+
+var tasks;
+
+
+
+
+function removeTask(TaskName){
+
+}
+
+function addTask(TaskName, TaskDescription){
+
+}
+
+function start(){
+    console.log("Server started on port " + PORT);
+    tasks = JSON.parse(fs.readFileSync('tasks.json', 'utf8'));
+    conse.log(tasks);
+}
+
+start();
